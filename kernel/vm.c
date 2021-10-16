680119ec848ae17f70c83a731e805508be0ecd2b
AndrejKovacik57 <xkovacika1@stuba.sk>
2021-10-16 16:52:19 +0200

    implement print page tables

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+recursive_vmprint(pagetable_t pagetable, int level)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    // entry is valid
+    if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+
+      for (int x = level; x <= 2 ; x++)
+        printf(" ..");
+
+      printf("%d: pte %p pa %p\n", i, pte, child);
+
+      //check if pte doesnt have any of the flags
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0)
+        // this PTE points to a lower-level page table.
+        recursive_vmprint((pagetable_t)child, level-1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  recursive_vmprint(pagetable, 2);
+}
